90084e8adcab86e61bb06f1234c6da736f221427
Wojciech Gawronski <afronski@gmail.com>
2014-07-21 12:26:13 +0200

    Improving readability of this file.

--- examples/storm-starter/src/clj/storm/starter/clj/word_count.clj
+++ examples/storm-starter/src/clj/storm/starter/clj/word_count.clj
@@ -1,23 +1,9 @@
-;; Licensed to the Apache Software Foundation (ASF) under one
-;; or more contributor license agreements.  See the NOTICE file
-;; distributed with this work for additional information
-;; regarding copyright ownership.  The ASF licenses this file
-;; to you under the Apache License, Version 2.0 (the
-;; "License"); you may not use this file except in compliance
-;; with the License.  You may obtain a copy of the License at
-;;
-;; http://www.apache.org/licenses/LICENSE-2.0
-;;
-;; Unless required by applicable law or agreed to in writing, software
-;; distributed under the License is distributed on an "AS IS" BASIS,
-;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-;; See the License for the specific language governing permissions and
-;; limitations under the License.
-
 (ns storm.starter.clj.word-count
   (:import [backtype.storm StormSubmitter LocalCluster])
   (:use [backtype.storm clojure config])
   (:gen-class))
+
+;; Spouts
 
 (defspout sentence-spout ["sentence"]
   [conf context collector]
@@ -41,6 +27,8 @@
   (Thread/sleep 500)
   (emit-spout! collector [(rand-nth sentences)]))
 
+;; Bolts
+
 (defbolt split-sentence ["word"] [tuple collector]
   (let [words (.split (.getString tuple 0) " ")]
     (doseq [w words]
@@ -59,8 +47,9 @@
          (ack! collector tuple)
          )))))
 
-(defn mk-topology []
+;; Topology
 
+(defn mk-topology []
   (topology
    {"1" (spout-spec sentence-spout)
     "2" (spout-spec (sentence-spout-parameterized
@@ -73,6 +62,8 @@
     "4" (bolt-spec {"3" ["word"]}
                    word-count
                    :p 6)}))
+
+;; Run configurations
 
 (defn run-local! []
   (let [cluster (LocalCluster.)]
@@ -87,6 +78,8 @@
    {TOPOLOGY-DEBUG true
     TOPOLOGY-WORKERS 3}
    (mk-topology)))
+
+;; Main
 
 (defn -main
   ([]
